c0b2df63e9e240ffe88e6fb72866c16a3531aa8d
Michihiro　Doi <48142612+michi-ddd@users.noreply.github.com>
2021-03-03 09:59:43 +0900

    顧客管理画面　修正 顧客詳細画面　追加

--- src/pages/customer/index.tsx
+++ src/pages/customer/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 import {
   Box,
   Card,
@@ -26,112 +27,142 @@ const Customer: React.FC = () => {
   }
   const customers = [
     {
-      id: 1,
-      name: 'name',
-      email: 'mail',
-      address: 'address',
-      phone: 'phone',
+      customer_id: 1,
+      customer_name: 'Shinichi Matoba',
+      email: 'xxxxx@gamil.com',
+      phone_number: '080xxxxxxxx',
+      sex: 'male',
+      address: '兵庫県神戸市西区xxxxxxxxxxxxxx',
+      birthday: '199x/xx/xx',
     },
     {
-      id: 2,
-      name: 'name',
-      email: 'mail',
-      address: 'address',
-      phone: 'phone',
+      customer_id: 2,
+      customer_name: 'Shinichi Matoba',
+      email: 'xxxxx@gamil.com',
+      phone_number: '080xxxxxxxx',
+      sex: 'male',
+      address: '兵庫県神戸市西区xxxxxxxxxxxxxx',
+      birthday: '199x/xx/xx',
     },
     {
-      id: 3,
-      name: 'name',
-      email: 'mail',
-      address: 'address',
-      phone: 'phone',
+      customer_id: 3,
+      customer_name: 'Shinichi Matoba',
+      email: 'xxxxx@gamil.com',
+      phone_number: '080xxxxxxxx',
+      sex: 'male',
+      address: '兵庫県神戸市西区xxxxxxxxxxxxxx',
+      birthday: '199x/xx/xx',
     },
     {
-      id: 4,
-      name: 'name',
-      email: 'mail',
-      address: 'address',
-      phone: 'phone',
+      customer_id: 4,
+      customer_name: 'Shinichi Matoba',
+      email: 'xxxxx@gamil.com',
+      phone_number: '080xxxxxxxx',
+      sex: 'male',
+      address: '兵庫県神戸市西区xxxxxxxxxxxxxx',
+      birthday: '199x/xx/xx',
     },
     {
-      id: 5,
-      name: 'name',
-      email: 'mail',
-      address: 'address',
-      phone: 'phone',
+      customer_id: 5,
+      customer_name: 'Shinichi Matoba',
+      email: 'xxxxx@gamil.com',
+      phone_number: '080xxxxxxxx',
+      sex: 'male',
+      address: '兵庫県神戸市西区xxxxxxxxxxxxxx',
+      birthday: '199x/xx/xx',
     },
     {
-      id: 6,
-      name: 'name6',
-      email: 'mail',
-      address: 'address',
-      phone: 'phone',
+      customer_id: 6,
+      customer_name: 'Michihiro Doi',
+      email: 'xxxxx@gamil.com',
+      phone_number: '080xxxxxxxx',
+      sex: 'male',
+      address: '兵庫県神戸市西区xxxxxxxxxxxxxx',
+      birthday: '199x/xx/xx',
     },
     {
-      id: 7,
-      name: 'name7',
-      email: 'mail',
-      address: 'address',
-      phone: 'phone',
+      customer_id: 7,
+      customer_name: 'Michihiro Doi',
+      email: 'xxxxx@gamil.com',
+      phone_number: '080xxxxxxxx',
+      sex: 'male',
+      address: '兵庫県神戸市西区xxxxxxxxxxxxxx',
+      birthday: '199x/xx/xx',
     },
     {
-      id: 8,
-      name: 'name8',
-      email: 'mail',
-      address: 'address',
-      phone: 'phone',
+      customer_id: 8,
+      customer_name: 'Ryosuke Kobayashi',
+      email: 'xxxxx@gamil.com',
+      phone_number: '080xxxxxxxx',
+      sex: 'male',
+      address: '兵庫県神戸市西区xxxxxxxxxxxxxx',
+      birthday: '198x/xx/xx',
     },
     {
-      id: 9,
-      name: 'name9',
-      email: 'mail',
-      address: 'address',
-      phone: 'phone',
+      customer_id: 9,
+      customer_name: 'Ryosuke Kobayashi',
+      email: 'xxxxx@gamil.com',
+      phone_number: '080xxxxxxxx',
+      sex: 'male',
+      address: '兵庫県神戸市西区xxxxxxxxxxxxxx',
+      birthday: '198x/xx/xx',
     },
     {
-      id: 10,
-      name: 'name10',
-      email: 'mail',
-      address: 'address',
-      phone: 'phone',
+      customer_id: 10,
+      customer_name: 'Ryosuke Kobayashi',
+      email: 'xxxxx@gamil.com',
+      phone_number: '080xxxxxxxx',
+      sex: 'male',
+      address: '兵庫県神戸市西区xxxxxxxxxxxxxx',
+      birthday: '198x/xx/xx',
     },
     {
-      id: 11,
-      name: 'name11',
-      email: 'mail',
-      address: 'address',
-      phone: 'phone',
+      customer_id: 11,
+      customer_name: 'Ryosuke Kobayashi',
+      email: 'xxxxx@gamil.com',
+      phone_number: '080xxxxxxxx',
+      sex: 'male',
+      address: '兵庫県神戸市西区xxxxxxxxxxxxxx',
+      birthday: '198x/xx/xx',
     },
     {
-      id: 12,
-      name: 'name12',
-      email: 'mail',
-      address: 'address',
-      phone: 'phone',
+      customer_id: 12,
+      customer_name: 'Ryosuke Kobayashi',
+      email: 'xxxxx@gamil.com',
+      phone_number: '080xxxxxxxx',
+      sex: 'male',
+      address: '兵庫県神戸市西区xxxxxxxxxxxxxx',
+      birthday: '198x/xx/xx',
     },
   ]
   return (
     <>
-      <GenericTemplate title="トップページ">
+      <GenericTemplate title="顧客管理">
         <Box m={3}>
           <Card>
             <TableContainer component={Paper}>
               <Table>
                 <TableHead>
                   <TableRow>
-                    <TableCell>Name</TableCell>
+                    <TableCell>氏名</TableCell>
                     <TableCell align="right">Email</TableCell>
-                    <TableCell align="right">Location</TableCell>
                     <TableCell align="right">Phone</TableCell>
+                    <TableCell align="right">性別</TableCell>
+                    <TableCell align="right">住所</TableCell>
+                    <TableCell align="right">生年月日</TableCell>
                   </TableRow>
                 </TableHead>
                 <TableBody>
                   {customers.slice(0, limit).map(customer => (
-                    <TableRow hover key={customer.id}>
-                      <TableCell>{customer.name}</TableCell>
+                    <TableRow hover key={customer.customer_id}>
+                      <TableCell>
+                        <Link to={`/customer/${customer.customer_id}`}>{customer.customer_name}</Link>
+                      </TableCell>
                       <TableCell align="right">{customer.email}</TableCell>
+                      <TableCell align="right">{customer.phone_number}</TableCell>
+                      <TableCell align="right">{customer.sex}</TableCell>
                       <TableCell align="right">{customer.address}</TableCell>
-                      <TableCell align="right">{customer.phone}</TableCell>
+                      <TableCell align="right">{customer.birthday}</TableCell>
                     </TableRow>
                   ))}
                 </TableBody>
