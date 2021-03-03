a0c4b7bda7ea437eb2e4f897a423be05f0fec3b1
Ryosuke Kobayashi <raartsmilk@gmail.com>
2021-03-03 11:51:24 +0900

    [add] RepositoryFactory 追加

---
+++ src/repositories/RepositoryFactory.ts
@@ -0,0 +1,15 @@
+import CustomerRepository from './customerRepository'
+
+type Repository = typeof CustomerRepository 
+
+type Repositories = {
+  [key: string]: Repository
+}
+
+const repositories: Repositories = {
+  customers: CustomerRepository,
+}
+
+export const RepositoryFactory = {
+  get: (name: string): Repository => repositories[name],
+}
