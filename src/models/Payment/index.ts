07635505ffc8d6a0bd2d5298fd3a61fe1f4b9dfe
Ryosuke Kobayashi <raartsmilk@gmail.com>
2021-03-04 22:39:51 +0900

    [fix] models/Payment createFromApi 追加

--- src/models/Payment/index.ts
+++ src/models/Payment/index.ts
@@ -1,3 +1,5 @@
+import * as Model from '../../models'
+
 export interface IPayment {
   readonly customerPaymentId: string
   readonly date: string
@@ -13,5 +15,13 @@ export class Payment implements IPayment {
     this.customerPaymentId = obj.customerPaymentId
     this.date = obj.date
     this.amount = obj.amount
+  }
+
+  static createFromApi(obj: Model.Api.Payment): Payment {
+    return new Payment({
+      customerPaymentId: obj.customer_payment_id,
+      date: obj.date,
+      amount: obj.amount
+    })
   }
 }
