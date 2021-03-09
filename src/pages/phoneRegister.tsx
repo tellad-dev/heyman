42c862cbd1b4c56655429620d23ab099d1cc0008
Michihiro　Doi <48142612+michi-ddd@users.noreply.github.com>
2021-03-09 22:47:43 +0900

    電話番号登録画面CSS適応

--- src/pages/phoneRegister.tsx
+++ src/pages/phoneRegister.tsx
@@ -2,31 +2,35 @@ import React, { useState } from 'react'
 import {
   Grid,
   Container,
-  Button,
   FormControlLabel,
   Checkbox,
+  Box,
 } from '@material-ui/core'
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles'
 import image from '../images/phone-register-image.png'
 import logo from '../images/logo-white.png'
+import SButton from '../components/atoms/Button'
+import BackspaceIcon from '@material-ui/icons/Backspace'
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
-      backgroundColor: '#00acee',
       height: '100vh',
+      backgroundColor: '#DCDCDC',
     },
     card: {
-      backgroundColor: 'white',
+      backgroundColor: '#2196F3',
     },
     cardContent: {
-      height: '20vh',
-      position: 'relative',
+      paddingTop: '10vh',
+      height: '10vh',
       textAlign: 'center',
-      fontSize: '2rem',
+      fontSize: '2.5rem',
+      fontWeight: 600,
     },
     cardMediaWrapper: {
-      height: '80vh',
+      paddingTop: '10vh',
+      height: '70vh',
     },
     cardImage: {
       maxHeight: '100%',
@@ -38,10 +42,10 @@ const useStyles = makeStyles((theme: Theme) =>
     container: {
       paddingTop: theme.spacing(4),
       paddingBottom: theme.spacing(4),
+      widht: '50%',
     },
-    numberButton: {
-      margin: theme.spacing(1),
-      backgroundColor: 'white',
+    checkLabel: {
+      color: 'white',
     },
   })
 )
@@ -71,7 +75,6 @@ const PhoneRegister: React.FC = () => {
   ): void => {
     phoneNumber !== null ? setPhoneNumber(phoneNumber.slice(0, -1)) : true
   }
-
   //
   // Jsx
   //
@@ -79,137 +82,224 @@ const PhoneRegister: React.FC = () => {
     <>
       <div className={classes.root}>
         <Grid container>
-          <Grid item xs={6} className={classes.card}>
-            <div className={classes.cardContent}>
-              お得なキャンペーンやお知らせを受け取ろう
-            </div>
-            <div className={classes.cardMediaWrapper}>
-              <img src={image} className={classes.cardImage} />
-            </div>
+          <Grid item xs={6}>
+            <Container maxWidth={'sm'}>
+              <div className={classes.cardContent}>
+                お得なキャンペーンや
+                <br />
+                お知らせを受け取ろう
+              </div>
+              <div className={classes.cardMediaWrapper}>
+                <img src={image} className={classes.cardImage} />
+              </div>
+            </Container>
           </Grid>
-          <Grid item xs={6} container>
-            <Container className={classes.container}>
-              <Grid item xs={12}>
+          <Grid item xs={6} className={classes.card}>
+            <Container maxWidth={'xs'} className={classes.container}>
+              <Box display="flex" justifyContent="center">
                 <img src={logo} className={classes.headerLogo} />
-              </Grid>
-              <Grid item xs={12}>
-                <h1>{phoneNumber ? phoneNumber : '電話番号'}</h1>
-              </Grid>
-              <Grid item xs={12} container>
-                <Grid item xs={4}>
-                  <Button
-                    className={classes.numberButton}
+              </Box>
+              <Box m={3} display="flex" justifyContent="center">
+                <SButton
+                  width={'100%'}
+                  height={'3rem'}
+                  fontSize={'1.5rem'}
+                  color={'#eee'}
+                  backgroundColor={'#1976D2'}
+                  margin={'auto'}
+                  borderRadius={'30px'}
+                  disabled
+                >
+                  {phoneNumber ? phoneNumber : '電話番号'}
+                </SButton>
+              </Box>
+              <Box display="flex" flexDirection="column">
+                <Box display="flex" justifyContent="center">
+                  <SButton
+                    width={'75px'}
+                    height={'75px'}
+                    color={'#eee'}
+                    fontSize={'2.5rem'}
+                    backgroundColor={'#1976D2'}
+                    margin={'auto'}
+                    borderRadius={'50%'}
                     onClick={(e) => addNumber(e, '1')}
                   >
                     1
-                  </Button>
-                </Grid>
-                <Grid item xs={4}>
-                  <Button
-                    className={classes.numberButton}
+                  </SButton>
+
+                  <SButton
+                    width={'75px'}
+                    height={'75px'}
+                    color={'#eee'}
+                    fontSize={'2.5rem'}
+                    backgroundColor={'#1976D2'}
+                    margin={'auto'}
+                    borderRadius={'50%'}
                     onClick={(e) => addNumber(e, '2')}
                   >
                     2
-                  </Button>
-                </Grid>
-                <Grid item xs={4}>
-                  <Button
-                    className={classes.numberButton}
+                  </SButton>
+
+                  <SButton
+                    width={'75px'}
+                    height={'75px'}
+                    color={'#eee'}
+                    fontSize={'2.5rem'}
+                    backgroundColor={'#1976D2'}
+                    margin={'auto'}
+                    borderRadius={'50%'}
                     onClick={(e) => addNumber(e, '3')}
                   >
                     3
-                  </Button>
-                </Grid>
-              </Grid>
-              <Grid item xs={12} container>
-                <Grid item xs={4}>
-                  <Button
-                    className={classes.numberButton}
+                  </SButton>
+                </Box>
+              </Box>
+              <Box mt={5} display="flex" flexDirection="column">
+                <Box display="flex" justifyContent="center">
+                  <SButton
+                    width={'75px'}
+                    height={'75px'}
+                    color={'#eee'}
+                    fontSize={'2.5rem'}
+                    backgroundColor={'#1976D2'}
+                    margin={'auto'}
+                    borderRadius={'50%'}
                     onClick={(e) => addNumber(e, '4')}
                   >
                     4
-                  </Button>
-                </Grid>
-                <Grid item xs={4}>
-                  <Button
-                    className={classes.numberButton}
+                  </SButton>
+
+                  <SButton
+                    width={'75px'}
+                    height={'75px'}
+                    color={'#eee'}
+                    fontSize={'2.5rem'}
+                    backgroundColor={'#1976D2'}
+                    margin={'auto'}
+                    borderRadius={'50%'}
                     onClick={(e) => addNumber(e, '5')}
                   >
                     5
-                  </Button>
-                </Grid>
-                <Grid item xs={4}>
-                  <Button
-                    className={classes.numberButton}
+                  </SButton>
+
+                  <SButton
+                    width={'75px'}
+                    height={'75px'}
+                    color={'#eee'}
+                    fontSize={'2.5rem'}
+                    backgroundColor={'#1976D2'}
+                    margin={'auto'}
+                    borderRadius={'50%'}
                     onClick={(e) => addNumber(e, '6')}
                   >
                     6
-                  </Button>
-                </Grid>
-              </Grid>
-              <Grid item xs={12} container>
-                <Grid item xs={4}>
-                  <Button
-                    className={classes.numberButton}
+                  </SButton>
+                </Box>
+              </Box>
+              <Box mt={5} display="flex" flexDirection="column">
+                <Box display="flex" justifyContent="center">
+                  <div></div>
+                  <SButton
+                    width={'75px'}
+                    height={'75px'}
+                    color={'#eee'}
+                    fontSize={'2.5rem'}
+                    backgroundColor={'#1976D2'}
+                    margin={'auto'}
+                    borderRadius={'50%'}
                     onClick={(e) => addNumber(e, '7')}
                   >
                     7
-                  </Button>
-                </Grid>
-                <Grid item xs={4}>
-                  <Button
-                    className={classes.numberButton}
+                  </SButton>
+
+                  <SButton
+                    width={'75px'}
+                    height={'75px'}
+                    color={'#eee'}
+                    fontSize={'2.5rem'}
+                    backgroundColor={'#1976D2'}
+                    margin={'auto'}
+                    borderRadius={'50%'}
                     onClick={(e) => addNumber(e, '8')}
                   >
                     8
-                  </Button>
-                </Grid>
-                <Grid item xs={4}>
-                  <Button
-                    className={classes.numberButton}
+                  </SButton>
+
+                  <SButton
+                    width={'75px'}
+                    height={'75px'}
+                    color={'#eee'}
+                    fontSize={'2.5rem'}
+                    backgroundColor={'#1976D2'}
+                    margin={'auto'}
+                    borderRadius={'50%'}
                     onClick={(e) => addNumber(e, '9')}
                   >
                     9
-                  </Button>
-                </Grid>
-              </Grid>
-              <Grid item xs={12} container>
-                <Grid item xs={4}></Grid>
-                <Grid item xs={4}>
-                  <Button
-                    className={classes.numberButton}
+                  </SButton>
+                </Box>
+              </Box>
+              <Box mt={5} display="flex" flexDirection="column">
+                <Box display="flex" justifyContent="center">
+                  <SButton
+                    width={'75px'}
+                    height={'75px'}
+                    color={'#2196F3'}
+                    backgroundColor={'#2196F3'}
+                    margin={'auto'}
+                    borderRadius={'50%'}
+                    disabled
+                  >
+                    -
+                  </SButton>
+                  <SButton
+                    width={'75px'}
+                    height={'75px'}
+                    color={'#eee'}
+                    fontSize={'2.5rem'}
+                    backgroundColor={'#1976D2'}
+                    margin={'auto'}
+                    borderRadius={'50%'}
                     onClick={(e) => addNumber(e, '0')}
                   >
                     0
-                  </Button>
-                </Grid>
-                <Grid item xs={4}>
-                  <Button
-                    className={classes.numberButton}
+                  </SButton>
+                  <SButton
+                    width={'75px'}
+                    height={'75px'}
+                    color={'#eee'}
+                    backgroundColor={'#1976D2'}
+                    margin={'auto'}
+                    borderRadius={'50%'}
                     onClick={deleteOneCharacter}
                   >
-                    ×
-                  </Button>
-                </Grid>
-              </Grid>
-              <Grid item xs={12}>
+                    <BackspaceIcon />
+                  </SButton>
+                </Box>
+              </Box>
+              <Box mt={1}>
                 <FormControlLabel
                   control={
-                    <Checkbox name="checkedB" color="primary" required />
+                    <Checkbox name="checkedB" color={'default'} required />
                   }
-                  label="利用規約に同意する"
+                  label="利用規約・プライバシーポリシーに同意する"
+                  className={classes.checkLabel}
                 />
-              </Grid>
-              <Grid item xs={12}>
-                <Button
-                  type="submit"
-                  fullWidth
-                  variant="contained"
-                  disabled={!phoneNumber}
+              </Box>
+              <Box mt={1}>
+                <SButton
+                  width={'100%'}
+                  height={'3rem'}
+                  color={'#eee'}
+                  fontSize={'1.5rem'}
+                  backgroundColor={'#1976D2'}
+                  margin={'auto'}
+                  borderRadius={'30px'}
                 >
                   受け取る
-                </Button>
-              </Grid>
+                </SButton>
+              </Box>
             </Container>
           </Grid>
         </Grid>
